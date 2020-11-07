37af1cf0e393a0bfdc1aeecb424f5cabd09d4da9
Alex Hoyau <a.hoyau@silexlabs.org>
2020-11-07 22:28:38 +0100

    fix tests

--- server/ForestryPublish.test.js
+++ server/ForestryPublish.test.js
@@ -1,15 +1,16 @@
-const PublishBase = require('./PublishBase')
-const data = require('./PublishBase.test.json')
+const ForestryPublish = require('./ForestryPublish')
+const data = require('./sample.json')
 
 const NUM_PAGES = 2
+const NUM_ADDED_FIELDS = 2 // we add the fields: layout and permalink
 const WITH_FORESTRY_CSS_CLASS = 'with-forestry'
 const NO_FORESTRY_CSS_CLASS = 'no-forestry'
 
 test('Generate forestry template', () => {
-  expect(PublishBase.toForestryTemplate).not.toBeNull()
+  expect(ForestryPublish.toForestryTemplate).not.toBeNull()
   expect(data).not.toBeNull()
   expect(data.pages).toHaveLength(NUM_PAGES)
-  const forestryTemplates = PublishBase.toForestryTemplates(data)
+  const forestryTemplates = ForestryPublish.toForestryTemplates(data)
   expect(forestryTemplates).not.toBeNull()
   expect(forestryTemplates).toHaveLength(NUM_PAGES)
 
@@ -18,7 +19,7 @@ test('Generate forestry template', () => {
   expect(page1.name).toBe('page-1.yml')
   expect(page1.label).toBe('Page 1')
   expect(page1.fields).not.toBeNull()
-  expect(page1.fields).toHaveLength(2)
+  expect(page1.fields).toHaveLength(2 + NUM_ADDED_FIELDS)
   
   const el1 = page1.fields[0]
   expect(el1).not.toBeNull()
